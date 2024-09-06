916ede0e11aaecbe9b23dbc086f7bf312c894d97
feri sandria <feri.chess@gmail.com>
2024-09-06 12:58:38 +0700

    menggunakan __str__ untuk memanggil object tanpa argument

--- OOP/class.py
+++ OOP/class.py
@@ -6,6 +6,9 @@ class phone:
         self.batrai = batrai
         self.cpu = cpu
 
+    def __str__(self):
+        return f"nama hp: {self.name}\nsistem operasi hp: {self.os}\nbatrai hp: {self.batrai}\ncpu hp: {self.cpu}"
+
 
 xiomi = phone("Redmi Note 8", "MIUI 10", "3000 mAh", "snapdragon 665")
 
@@ -13,3 +16,5 @@ print(f"nama hp: {xiomi.name}")
 print(f"sistem operasi hp: {xiomi.os}")
 print(f"batrai hp: {xiomi.batrai}")
 print(f"cpu hp: {xiomi.cpu}")
+
+print(f"---\n{xiomi}")
