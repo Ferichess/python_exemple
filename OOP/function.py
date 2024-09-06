a08b76afec2549e9f6c39b8da1d70a5999106461
feri sandria <feri.chess@gmail.com>
2024-09-06 15:58:08 +0700

    instance function lowbat

--- OOP/function.py
+++ OOP/function.py
@@ -1,14 +1,22 @@
 class Phone:
+    sim = "dual sim card"
+
     def __init__(self, name, os, battery, cpu) -> None:
         self.name = name
         self.os = os
         self.battery = battery
         self.cpu = cpu
 
+    def lowbat(self, battery_low):
+        if battery_low <= 10:
+            return f"{self.name} low battery {battery_low} %"
+
     def __str__(self) -> str:
-        return f"nama hp: {self.name}\nsistem operasi hp: {self.os}\nbatrai hp: {self.battery}\ncpu hp: {self.cpu}"
+        return f"nama hp: {self.name}\nsistem operasi hp: {self.os}\nbatrai hp: {self.battery}\ncpu hp: {self.cpu}\nsim hp: {self.sim}"
 
 
 Redmi = Phone("Redmi Note 8", "MIUI 10", "3000 mAh", "snapdragon 665")
 
-print(Redmi)
+print(f"-1-\n{Redmi}")
+print(f"-2-\n{Redmi.lowbat(12)}")
+# Redmi.lowbat(12)
